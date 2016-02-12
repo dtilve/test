302cc5aa6730ddaa56d496709b2d0ea484cb7fed
dtilve <tilve.dana@gmail.com>
2016-02-12 11:34:37 -0300

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Dana Neyra on 2/12/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Dana Neyra on 2/12/16.
+//  Copyright © 2016 Dana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
